0de09fd8fb994623b456cefef03b1dc20c6df621
Oleksndr Levadnyi <iminib39@gmail.com>
2020-04-07 15:20:36 +0300

    Created a spring boot project

---
+++ src/main/java/com/khpi/economic/EconomicApplication.java
@@ -0,0 +1,13 @@
+package com.khpi.economic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khpi/economic/EconomicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khpi.economic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
